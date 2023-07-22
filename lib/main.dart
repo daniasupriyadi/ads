92024e115d0f0c535fa35493ba4387def31c743c
daniasupriyadi <daniasupriyadi2106@gmail.com>
2023-07-22 17:17:04 +0700

    change main

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlue,
         fontFamily: 'Nunito',
       ),
-      home: ForgotPage(),
+      home: SplashScreen(),
       routes: routes,
     );
   }
